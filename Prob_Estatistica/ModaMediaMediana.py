a3f85bb46d97f74687cf3d9bee85be358690b69e
Rafael Reis <contato.rafael.reis41@gmail.com>
2026-04-10 01:10:08 -0300

    início da lógica matemática do projeto

---
+++ Prob_Estatistica/ModaMediaMediana.py
@@ -0,0 +1,13 @@
+def calcular_moda(v):
+    pass
+
+def calcular_mediana(v):
+    pass
+
+def calcular_media(v):
+    
+    calc = sum(v)/len(v)
+    return calc
+
+valores = []
+calcular_media(valores)
